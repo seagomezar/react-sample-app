1a40d0a7d11e42bfa81f7927c7b3c5dba259beab
Sebastian Gomez <seagomezar@gmail.com>
2022-10-06 11:24:45 -0500

    First Modification to review the status on deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
-  return <div className="App">
-      <img src="https://paketo.io/images/paketo-logo-full-color.png" alt="Paketo Buildpacks logo"></img>
+  return (
+    <div className="App">
+      <img
+        src="https://paketo.io/images/paketo-logo-full-color.png"
+        alt="Paketo Buildpacks logo"
+      ></img>
       Powered by Paketo Buildpacks
-  </div>;
+      <h1>First Modification</h1>
+    </div>
+  );
 }
 
 export default App;
